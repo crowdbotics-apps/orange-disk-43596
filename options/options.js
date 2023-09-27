96550f0a3120ff72135f340a4e48c3d684e5c67f
Crowdbotics <team@crowdbotics.com>
2023-09-27 11:31:44 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "orange-disk-43596",
+  url: "https://orange-disk-43596.botics.co",
+  api: "https://orange-disk-43596.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
